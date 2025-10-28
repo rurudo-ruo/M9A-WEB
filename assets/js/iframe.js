b5c487579b99e7b080af57d69a2cdbb85d68dd2e
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2025-10-28 14:03:18 +0800

    chore: 清空文档站

--- assets/js/iframe.js
+++
@@ -1,5 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const hasIframeParam = urlParams.has('iframe');
-if (!hasIframeParam) {
-  window.location.href = `/#${window.location.pathname}`;
-}
